dff1d3f4d1bd28ca8eec0cee1cc05b15b893e56c
HarikaYarlagadda <hariyhari1999@gmail.com>
2020-10-21 14:58:47 +0530

    added percentage of winning

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,8 +1,19 @@
-#!/bin/bash -x
+#! /bin/bash -x
+tailcount=0
+headcount=0
+for (( i=0 ; i<100 ; i++ ))
+do
 coin=$(( RANDOM%2 ))
 if (( $coin == 0 ))
 then
 	echo "tails"
+	(( tailcount++ ))
 else
 	echo "heads"
+	(( headcount++ ))
 fi
+done
+echo "no of times heads came $headcount"
+echo "no of times tails came $tailcount"
+headspercent=$(($headcount/1))
+tailspercent=$(($tailcount/1))
